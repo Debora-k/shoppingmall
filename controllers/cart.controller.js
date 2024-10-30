2ea73af8da8d92301a4a2ed79d324cd84770a5b4
Debora-k <debora0104k@gmail.com>
2024-10-30 01:15:53 -0600

    delete cart items and change item's qty

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,4 +1,5 @@
 const Cart = require("../models/Cart");
+const Product = require("../models/Product");
 
 const cartController = {}
 
@@ -20,6 +21,61 @@ cartController.addItemToCart = async(req,res)=> {
         await cart.save();
         res.status(200).json({status:"success", data:cart, cartItemQty:cart.items.length});
     } catch(error){
+        return res.status(400).json({status:"failed", error:"Something went wrong. Please try again"});
+    }
+};
+
+cartController.getCart = async(req,res) => {
+    try{
+        const {userId} = req; //from middleware
+        let cart = await Cart.findOne({userId});
+        if(!cart) {
+            cart = new Cart({userId});
+            await cart.save();
+        }
+        await cart.populate({
+            path:"items",
+            populate:{
+                path:"productId",
+                model:"Product",
+            }
+        });
+        res.status(200).json({status:"success", data:cart.items});
+    } catch(error){
+        return res.status(400).json({status:"failed", error:error.message});
+    }
+};
+
+cartController.deleteCartItem = async(req,res) => {
+    try{
+        const {userId} = req; 
+        const {id} = req.params;
+        let cart = await Cart.findOne({userId});
+        cart.items.id(id).deleteOne();
+        await cart.save();
+        res.status(200).json({status:"success", data:cart.items, cartItemQty:cart.items.length});
+    } catch(error){
+        return res.status(400).json({status:"failed", error:error.message});
+    }
+};
+
+cartController.updateQty = async(req,res) => {
+    try{
+        const {userId} = req;
+        const {size, qty} = req.body;
+        const {id} = req.params;
+        let cart = await Cart.findOne({userId});
+        if(!cart) throw new Error("There is no cart for this user.");
+        const item = cart.items.find((i)=>i._id.equals(id));
+        const product = await Product.findById(item.productId);
+        
+        if(qty > product.stock[size]) {
+            throw new Error("Sorry, there is not enough stock.");
+        }
+        item.qty = qty;
+        await cart.save();
+        res.status(200).json({status:"success", data:cart.items, cartItemQty:cart.items.length});
+    }catch(error){
         return res.status(400).json({status:"failed", error:error.message});
     }
 };
